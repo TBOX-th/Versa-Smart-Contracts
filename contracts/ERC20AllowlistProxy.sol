bd2c98ffa4dc433d7e96c72c4571720ce68ad548
Alexandru <alexandru@tbox.net>
2023-08-04 14:01:38 +0700

    Initial commit.

--- contracts/ERC20AllowlistProxy.sol
+++ contracts/ERC20AllowlistProxy.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Versa Contracts v1.0.0 (/contracts/ERC20AllowlistProxy.sol)
-pragma solidity 0.8.20;
+pragma solidity >=0.4.22 <0.9.0;
 
 import "./AllowlistRegistry.sol";
 
--- contracts/InvestmentToken.sol
+++ contracts/InvestmentToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Versa Contracts v1.0.0 (/contracts/InvestmentToken.sol)
-pragma solidity 0.8.20;
+pragma solidity >=0.4.22 <0.9.0;
 
 import "./ERC20AllowlistProxy.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
